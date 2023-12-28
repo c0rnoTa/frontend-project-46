ce7959bb69046b77449ab988b7dad3c831fb30ce
Anton Zakharov <hello@antonzakharov.ru>
2023-12-28 13:58:30 +0300

    works first diff

--- src/diff.js
+++ src/diff.js
@@ -1,9 +1,41 @@
 import _ from 'lodash';
 
-export default (obj1, obj2) => {
-  const src = Object.entries(obj1);
-  const dst = Object.entries(obj2);
-  src.sort();
-  dst.sort();
-  return `${src[0]}\n${dst[0]}`;
+const stateAdd = 'add';
+const stateRemove = 'remove';
+const stateSame = 'same';
+const stateChange = 'change';
+
+const addKey = (key, value) => [stateAdd, [key, value]];
+
+const removeKey = (key, value) => [stateRemove, [key, value]];
+
+const compareValues = (key, newValue, oldValue) => {
+  if (newValue === oldValue) {
+    return [stateSame, [key, newValue]];
+  }
+  return [stateChange, [key, [oldValue, newValue]]];
 };
+
+const genDiff = (obj1, obj2) => {
+  const res = _.entries({ ...obj1, ...obj2 });
+  // const res = _.entries(_.merge(obj1, obj2));
+  res.sort();
+  const diff = res.map(([key, value]) => {
+    if (!(key in obj1)) {
+      return addKey(key, value);
+    }
+    if (!(key in obj2)) {
+      return removeKey(key, value);
+    }
+    return compareValues(key, value, obj1[key]);
+  });
+  return diff;
+};
+
+export {
+  genDiff,
+  stateAdd,
+  stateRemove,
+  stateSame,
+  stateChange,
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import parseToString from './parser.js';
-import genDiff from './diff.js';
+import * as diff from './diff.js';
 
 export default (filepath1, filepath2) => {
-  console.log(genDiff(parseToString(filepath1), parseToString(filepath2)));
+  const result = diff.genDiff(parseToString(filepath1), parseToString(filepath2));
+  console.log('{');
+  result.forEach(([state, [key, value]]) => {
+    switch (state) {
+      case diff.stateAdd:
+        console.log(`  + ${key}: ${value}`);
+        break;
+      case diff.stateRemove:
+        console.log(`  - ${key}: ${value}`);
+        break;
+      case diff.stateChange:
+        console.log(`  - ${key}: ${value[0]}`);
+        console.log(`  + ${key}: ${value[1]}`);
+        break;
+      default:
+        console.log(`    ${key}: ${value}`);
+    }
+  });
+  console.log('}');
 };
