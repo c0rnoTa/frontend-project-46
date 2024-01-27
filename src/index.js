47da1ef1391cb5ea89723d3ded116138a7fc3a40
Anton Zakharov <hello@antonzakharov.ru>
2024-01-27 23:27:23 +0300

    Add formater option

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import parseToObject from './parser.js';
 import { genDiff } from './diff.js';
 import getStylish from './formaters/stylish.js';
 
-const printDiff = (filepath1, filepath2) => {
+const printDiff = (filepath1, filepath2, opts) => {
   const ast = genDiff(parseToObject(filepath1), parseToObject(filepath2));
   // console.log(JSON.stringify(ast, null, 2));
-  console.log(getStylish(ast));
+  switch (opts.format) {
+    case 'stylish':
+      console.log(getStylish(ast));
+      break;
+    default:
+      console.log(`Unsupported formater ${opts.format}`);
+  }
 };
 
 export default printDiff;
