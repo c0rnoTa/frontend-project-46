d0bc3dd1af1baba6b580a14c6e96197dfeba4f75
Anton Zakharov <hello@antonzakharov.ru>
2024-01-28 16:52:37 +0300

    wip stylish fp

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,28 +5,23 @@ const indentCount = 4;
 
 const getStylish = (ast, level = 1) => {
   const fmtString = (node, lvl, char = ' ', attr = 'value') => {
-    let result = `${indent.repeat(indentCount * lvl - 2)}${char} ${node.key}: `;
-    result += ((node[attr] instanceof Object) ? getStylish(node[attr], lvl + 1) : node[attr]);
+    const result = `${indent.repeat(indentCount * lvl - 2)}${char} ${node.key}: ` 
+      +((node[attr] instanceof Object) ? getStylish(node[attr], lvl + 1) : node[attr]);
     return result;
   };
 
   const result = ast.map(
     (node) => {
-      let str = '';
       switch (node.state) {
         case diff.stateAdd:
-          str = fmtString(node, level, '+');
-          break;
+          return fmtString(node, level, '+');
         case diff.stateRemove:
-          str = fmtString(node, level, '-');
-          break;
+          return fmtString(node, level, '-');
         case diff.stateChanged:
-          str = `${fmtString(node, level, '-', 'oldData')}\n${fmtString(node, level, '+', 'newData')}`;
-          break;
+          return `${fmtString(node, level, '-', 'oldData')}\n${fmtString(node, level, '+', 'newData')}`;
         default:
-          str = fmtString(node, level);
+          return fmtString(node, level);
       }
-      return str;
     },
   ).join('\n');
   return `{\n${result}\n${indent.repeat(indentCount * (level - 1))}}`;
