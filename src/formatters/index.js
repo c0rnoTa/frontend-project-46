09c54e4ebcd54462d300d86bcb731facd9694f36
Anton Zakharov <hello@antonzakharov.ru>
2024-02-13 15:38:34 +0300

    return object as formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,14 +1,8 @@
 import getStylish from './stylish.js';
 import getPlain from './plain.js';
-import getJSON from './json.js';
 
-export default (format) => {
-  switch (format) {
-    case 'json':
-      return getJSON;
-    case 'plain':
-      return getPlain;
-    default:
-      return getStylish;
-  }
+export default {
+  json: JSON.stringify,
+  plain: getPlain,
+  stylish: getStylish,
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import parseToObject from './parser.js';
 import genDiff from './diff.js';
-import getFormatter from './formatters/index.js';
+import formats from './formatters/index.js';
 import * as file from './files.js';
 
 const getDiff = (filepath1, filepath2, format) => {
@@ -11,8 +11,7 @@ const getDiff = (filepath1, filepath2, format) => {
   const object1 = parseToObject(fileContent1, fileType1);
   const object2 = parseToObject(fileContent2, fileType2);
   const ast = genDiff(object1, object2);
-  const formatter = getFormatter(format);
-  return formatter(ast);
+  return formats[format](ast);
 };
 
 export default getDiff;
